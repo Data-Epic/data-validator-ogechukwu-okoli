907ce0a1dfbe97aba74b90cc1e3cdaee70aa9556
Human-Gechi <kimberlydaniel363@gmail.com>
2025-03-14 23:37:25 +0100

    pushing code

---
+++ datavalidator/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name= "datavalidator",
+    version="0.1",
+    packages=find_packages(),
+    install_requires= [],
+    author= "Okoli Ogechukwu Abimbola",
+    author_email="okoliogechi74@gmail.com",
+    description="A simple data validation package"
+)
