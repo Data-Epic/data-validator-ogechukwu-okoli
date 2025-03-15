9b1e951fb9df1c9749df865c239faf288eb5280e
Human-Gechi <kimberlydaniel363@gmail.com>
2025-03-15 03:27:35 +0100

    changes

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 setup(
-    name= "datavalidator",
+    name= "datavalidatorpro",
     version="0.1",
+    license="MIT",
     packages=find_packages(),
     install_requires= [],
     author= "Okoli Ogechukwu Abimbola",
     author_email="okoliogechi74@gmail.com",
-    description="A simple data validation package"
+    description="A simple data validation package",
+
+    classifiers=[
+        "Programming language:: Python ::3.13.2",
+        "license :: OSI Approved :: MIT License"
+        ],
 )
